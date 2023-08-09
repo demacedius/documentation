d864b4746be78df0f9fcfdbe91dac53e32b79e5a
demacedius <demacedius@gmail.com>
2023-08-09 07:57:40 +0200

    first commit

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,81 +1,43 @@
 <script>
-  import { T } from '@threlte/core'
-  import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras'
+	// @ts-nocheck
+
+	import { T, useFrame } from '@threlte/core';
+	import { interactivity, HTML } from '@threlte/extras';
+	import { spring } from 'svelte/motion';
+
+	import { CircleGeometry, DirectionalLight, MeshStandardMaterial } from 'three';
+
+	interactivity();
+	const scale = spring(1);
+
+	let rotation = 0;
+	useFrame((state, delta) => {
+		rotation += delta;
+	});
 </script>
 
 <T.PerspectiveCamera
-  makeDefault
-  position={[-10, 10, 10]}
-  fov={15}
->
-  <OrbitControls
-    autoRotate
-    enableZoom={false}
-    enableDamping
-    autoRotateSpeed={0.5}
-    target.y={1.5}
-  />
-</T.PerspectiveCamera>
-
-<T.DirectionalLight
-  intensity={0.8}
-  position.x={5}
-  position.y={10}
-/>
-<T.AmbientLight intensity={0.2} />
-
-<Grid
-  position.y={-0.001}
-  cellColor="#ffffff"
-  sectionColor="#ffffff"
-  sectionThickness={0}
-  fadeDistance={25}
-  cellSize={2}
+	makeDefault
+	position={[10, 10, 10]}
+	on:create={({ ref }) => {
+		ref.lookAt(0, 1, 0);
+	}}
 />
 
-<ContactShadows
-  scale={10}
-  blur={2}
-  far={2.5}
-  opacity={0.5}
-/>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
+<T.DirectionalLight position={[0, 10, 10]} castShadow />
+<T.Mesh
+	rotation.y={rotation}
+	position.y={1}
+	scale={$scale}
+	on:pointerenter={() => scale.set(1.5)}
+	on:pointerleave={() => scale.set(1)}
+	castShadow
 >
-  <T.Mesh
-    position.y={1.2}
-    position.z={-0.75}
-  >
-    <T.BoxGeometry />
-    <T.MeshStandardMaterial color="#0059BA" />
-  </T.Mesh>
-</Float>
+	<T.BoxGeometry args={[1, 2, 1]} />
+	<T.MeshStandardMaterial color="green" />
+</T.Mesh>
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[1.2, 1.5, 0.75]}
-    rotation.x={5}
-    rotation.y={71}
-  >
-    <T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
-    <T.MeshStandardMaterial color="#F85122" />
-  </T.Mesh>
-</Float>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[-1.4, 1.5, 0.75]}
-    rotation={[-5, 128, 10]}
-  >
-    <T.IcosahedronGeometry />
-    <T.MeshStandardMaterial color="#F8EBCE" />
-  </T.Mesh>
-</Float>
+<T.Mesh rotation.x={-Math.PI / 2} receiveShadow>
+	<T.CircleGeometry args={[4, 40]} />
+	<T.MeshStandardMaterial color="white" />
+</T.Mesh>
